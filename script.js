b4502c6caa8f0088349ba5a3fa2cc233cec831cb
Manoj <krishnamanoj86@gmail.com>
2023-07-10 19:28:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openHome(){
+    window.open("index.html", "_self");
+}
+function openAbout(){
+    window.open("about.html", "_self");
+}
+function openSkill(){
+    window.open("skill.html", "_self");
+}
+function sendEmail(){
+    var email = 'sk21eeb0b52@student.nitw.ac.in';
+
+    var mailtoLink = 'mailto:' + email;
+    window.location.href = mailtoLink;
+}
